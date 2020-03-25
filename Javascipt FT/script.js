a9f41c5592651b4b0c315d704a1418561f5e108f
kedmunds93 <nwankwopromise@ymail.com>
2020-03-26 00:42:34 +0100

    Javascript-FT

---
+++ Javascipt FT/script.js
@@ -0,0 +1,10 @@
+var name = "Promise Nwankwo";
+var courses = ["HTML", "CSS", "JavaScript"]; 
+
+console.log('My name is: ' + name);
+console.log('Courses: ' + courses);
+
+for (let i =1 ; i <= 200; i++) {
+   if (i % 2 !== 0) console.log(i);
+
+}
